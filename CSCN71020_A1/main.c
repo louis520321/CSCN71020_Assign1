4c494aeaf6aaebdb7c84909e6211256171bfafc2
louis <lhe5921@conestogac.on.ca>
2022-10-25 14:53:00 -0400

    Fix the subtract problem and every things working now

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -20,6 +21,10 @@ void main() {
 	{
 	case 1:
 		add();
+		break;
+	case 2:
+		subtract();
+		break;
 	}
 
 }
@@ -38,10 +43,22 @@ void printOptions() {
 
 void add() {
 	double num1, num2, result;
-	printf("Enter the first value:");
+	printf("Enter the first value:  \n");
 	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
+	printf("Enter the second value:  \n");
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+
+void subtract()
+{
+	double num1, num2, result;
+	printf("Enter the first value:  \n");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:  \n");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
